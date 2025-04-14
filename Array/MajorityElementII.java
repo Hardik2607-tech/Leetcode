5b803db40e6e977ad1b3162ebf2f5f6b17bd8d97
Hardik Saxena <Hardik.120776@stu.upes.ac.in>
2025-04-14 20:12:58 +0530

    Array

---
+++ Array/MajorityElementII.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class MajorityElementII {
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        int count1 = 0, count2 = 0;
+        Integer candidate1 = null, candidate2 = null;
+
+        // Step 1: Find candidates
+        for (int num : nums) {
+            if (candidate1 != null && num == candidate1) {
+                count1++;
+            } else if (candidate2 != null && num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Step 2: Verify candidates
+        count1 = count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        if (count1 > n / 3) result.add(candidate1);
+        if (count2 > n / 3) result.add(candidate2);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        MajorityElementII solution = new MajorityElementII();
+
+        System.out.println("Example 1: " + solution.majorityElement(new int[]{3, 2, 3})); // Output: [3]
+        System.out.println("Example 2: " + solution.majorityElement(new int[]{1}));       // Output: [1]
+        System.out.println("Example 3: " + solution.majorityElement(new int[]{1, 2}));    // Output: [1, 2]
+    }
+}
