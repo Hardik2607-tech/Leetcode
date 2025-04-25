43f296389da659f8e1b0d1a7d61a7d46081049c9
Hardik Saxena <Hardik.120776@stu.upes.ac.in>
2025-04-25 20:13:06 +0530

    Array

---
+++ Array/SearchRange.java
@@ -0,0 +1,51 @@
+public class SearchRange {
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        int[] result = searchRange(nums, target);
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = findFirst(nums, target);
+        result[1] = findLast(nums, target);
+        return result;
+    }
+
+    private static int findFirst(int[] nums, int target) {
+        int index = -1;
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+
+            if (nums[mid] == target) index = mid;
+        }
+
+        return index;
+    }
+
+    private static int findLast(int[] nums, int target) {
+        int index = -1;
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
+            if (nums[mid] == target) index = mid;
+        }
+
+        return index;
+    }
+}
