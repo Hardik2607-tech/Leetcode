6e27912d6144f392d31710f17359b3734a62822c
Hardik Saxena <Hardik.120776@stu.upes.ac.in>
2025-05-17 18:43:34 +0530

    Array

---
+++ Array/SortColors.java
@@ -0,0 +1,33 @@
+public class SortColors {
+    public static void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while (mid <= high) {
+            switch (nums[mid]) {
+                case 0:
+                    int temp0 = nums[low];
+                    nums[low] = nums[mid];
+                    nums[mid] = temp0;
+                    low++;
+                    mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    int temp2 = nums[mid];
+                    nums[mid] = nums[high];
+                    nums[high] = temp2;
+                    high--;
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        sortColors(nums);
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
