fb74677fbd78d1ef49ba8e0d6fb0a8301b2b1787
Hardik Saxena <Hardik.120776@stu.upes.ac.in>
2025-05-21 22:26:24 +0530

    Update tempCodeRunnerFile.java

--- Array/tempCodeRunnerFile.java
+++ Array/tempCodeRunnerFile.java
@@ -1,32 +1,53 @@
-import java.util.Arrays;
-
-public class CountPrimes {
-    public static int countPrimes(int n) {
-        if (n <= 2) return 0;
-
-        boolean[] isPrime = new boolean[n];
-        Arrays.fill(isPrime, true);
-        isPrime[0] = false;
-        isPrime[1] = false;
-
-        for (int i = 2; i * i < n; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j < n; j += i) {
-                    isPrime[j] = false;
-                }
-            }
-        }
-
-        int count = 0;
-        for (boolean prime : isPrime) {
-            if (prime) count++;
-        }
-
-        return count;
+public class MaximumGap {
+    public static void main(String[] args) {
+        int[] nums = {3, 6, 9, 1};
+        int result = maximumGap(nums);
+        System.out.println("Maximum gap: " + result);
     }
 
-    public static void main(String[] args) {
-        int n = 10;
-        System.out.println("Number of primes less than " + n + " is: " + countPrimes(n));
+    public static int maximumGap(int[] nums) {
+        if (nums == null || nums.length < 2)
+            return 0;
+
+        int min = nums[0], max = nums[0];
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        if (min == max)
+            return 0;
+
+        int n = nums.length;
+        int bucketSize = Math.max(1, (max - min) / (n - 1));
+        int bucketCount = (max - min) / bucketSize + 1;
+
+        int[] bucketMin = new int[bucketCount];
+        int[] bucketMax = new int[bucketCount];
+        boolean[] bucketUsed = new boolean[bucketCount];
+
+        for (int i = 0; i < bucketCount; i++) {
+            bucketMin[i] = Integer.MAX_VALUE;
+            bucketMax[i] = Integer.MIN_VALUE;
+        }
+
+        for (int num : nums) {
+            int idx = (num - min) / bucketSize;
+            bucketMin[idx] = Math.min(bucketMin[idx], num);
+            bucketMax[idx] = Math.max(bucketMax[idx], num);
+            bucketUsed[idx] = true;
+        }
+
+        int maxGap = 0;
+        int prevMax = min;
+
+        for (int i = 0; i < bucketCount; i++) {
+            if (!bucketUsed[i])
+                continue;
+            maxGap = Math.max(maxGap, bucketMin[i] - prevMax);
+            prevMax = bucketMax[i];
+        }
+
+        return maxGap;
     }
 }
