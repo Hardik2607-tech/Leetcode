79edb7e269a7f7bbb264ad8d09a3afaadad64f57
Hardik Saxena <Hardik.120776@stu.upes.ac.in>
2025-06-17 19:30:33 +0530

    Array

---
+++ Array/InsertionOfTwoArray.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class InsertionOfTwoArray {
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        Set<Integer> resultSet = new HashSet<>();
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                resultSet.add(num);
+            }
+        }
+
+        int[] result = new int[resultSet.size()];
+        int i = 0;
+        for (int num : resultSet) {
+            result[i++] = num;
+        }
+
+        return result;
+    }
+
+    // ✅ Add this main method to run the program
+    public static void main(String[] args) {
+        int[] nums1 = {4, 9, 5};
+        int[] nums2 = {9, 4, 9, 8, 4};
+
+        int[] result = intersection(nums1, nums2);
+        System.out.println("Intersection: " + Arrays.toString(result));
+    }
+}
